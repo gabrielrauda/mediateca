7df196d241b22614f2721e0b65c0b58863514030
gabrielrauda <gabrielrauda.adoc@gmail.com>
2025-04-24 00:22:51 -0600

    Create DVD.java

---
+++ DVD.java
@@ -0,0 +1,29 @@
+package Modelos;
+
+class DVD extends Materiales{
+    private String director;
+    private String duracion;
+    private String genero;
+
+    public DVD(Integer idMaterial, String titulo, String director, String duracion, String genero) {
+        super(idMaterial, titulo);
+        this.director = director;
+        this.duracion = duracion;
+        this.genero = genero;
+        
+}
+
+    public String getdirector() {
+        return director;
+    }
+
+    public String getduracion() {
+        return duracion;
+    }
+
+    public String getgenero() {
+        return genero;
+    }
+    
+    
+}

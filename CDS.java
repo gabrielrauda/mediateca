7ca5adad94745a01bbc2d8c1d95c40771b88e318
gabrielrauda <gabrielrauda.adoc@gmail.com>
2025-04-24 00:21:25 -0600

    Create CDS.java

---
+++ CDS.java
@@ -0,0 +1,41 @@
+package Modelos;
+
+class CDS extends Materiales{
+    private String artista;
+    private String genero;
+    private Integer duracion;
+    private Integer num_canciones;
+    private Integer stock;
+
+    public CDS(Integer idMaterial, String titulo, String artista, String genero, Integer duracion, Integer num_canciones,Integer stock) {
+        super(idMaterial, titulo);
+        this.artista = artista;
+        this.genero = genero;
+        this.duracion = duracion;
+        this.num_canciones = num_canciones;
+        this.stock = stock;
+        
+    }
+
+    public String getartista() {
+        return artista;
+    }
+
+    public String getgenero() {
+        return genero;
+    }
+
+    public Integer getduracion() {
+        return duracion;
+    }
+
+    public Integer getnum_canciones() {
+        return num_canciones;
+    }
+
+    public Integer getstock() {
+        return stock;
+    }
+    
+    
+}
